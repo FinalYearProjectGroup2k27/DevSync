ea9e6a7e4ad32c3ee1f69a45771b658ef2ce07bc
Amruth <137883418+Amruth53@users.noreply.github.com>
2026-08-16 18:47:33 +0000

    Add user services and Redis integration

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,30 @@
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
-import morgan from 'morgan'
-import cors from "cors";
-import connect from "./db/db.js";
-
+import express from 'express';
+import morgan from 'morgan';
+import connect from './db/db.js';
+import userRoutes from './routes/user.routes.js';
+//import projectRoutes from './routes/project.routes.js';
+//import aiRoutes from './routes/ai.routes.js';
+import cookieParser from 'cookie-parser';
+//import cors from 'cors';
 connect();
 
-const app=express();
 
-app.use(morgan("dev"));
-app.use(cors());
+const app = express();
 
+//app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/users', userRoutes);
+//app.use('/projects', projectRoutes);
+//app.use("/ai", aiRoutes)
 
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-
-
-export default app;
+export default app; 
