e74ff0a372825bea69a587507d080516c3ce91f4
Amruth <137883418+Amruth53@users.noreply.github.com>
2026-08-17 11:14:13 +0000

    Update authentication middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -24,7 +24,7 @@ export const authUser = async (req, res, next) => {
         next();
     } catch (error) {
 
-        console.log(error);git a
+        console.log(error);
 
         res.status(401).send({ error: 'Unauthorized User' });
     }
